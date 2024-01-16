7788394b9bf9e1f219bf84ed1a714eba94421d5b
Aderi <102830644+Adrixgamer78@users.noreply.github.com>
2024-01-16 02:47:41 +0100

    SquidCraft Launcher

--- build.js
+++ build.js
@@ -66,7 +66,7 @@ class Index {
                 appId: preductname,
                 productName: preductname,
                 copyright: 'Copyright © 2020-2024 Adrixgamer78',
-                artifactName: "Aderi.Launcher-${os}-${arch}.${ext}",
+                artifactName: "Squid.Craft.Launcher-${os}-${arch}.${ext}",
                 extraMetadata: { main: 'app/app.js' },
                 files: ["app/**/*", "package.json", "LICENSE.md"],
                 directories: { "output": "dist" },
--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -29,7 +29,7 @@ class Splash {
 
     async startAnimation() {
         let splashes = [
-            { "message": "Aderi Launcher", "author": "Adrixgamer78" },
+            { "message": "Squid Craft", "author": "Adrixgamer78" },
         ];
         let splash = splashes[Math.floor(Math.random() * splashes.length)];
         this.splashMessage.textContent = splash.message;
