4d0fabce49d4407a421cd76bd4cafa785ed5a499
Aderi <102830644+Adrixgamer78@users.noreply.github.com>
2024-01-19 14:10:25 +0100

    SquidCraft Launcher

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -82,7 +82,7 @@ class Splash {
             this.startLauncher();
         }).catch(e => {
             console.error(e);
-            return this.shutdown("No se ha detectado conexión a Internet,<br>por favor, inténtelo de nuevo más tarde.");
+            return this.shutdown("No se ha podido conectar al servidor,<br>por favor, inténtelo de nuevo más tarde.");
         })
     }
 
