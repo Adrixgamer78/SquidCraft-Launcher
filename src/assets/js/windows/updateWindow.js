fdad731c7fd1cac62dfca77b0081a346bdad1415
Aderi <102830644+Adrixgamer78@users.noreply.github.com>
2024-02-09 14:21:51 +0100

    v2.0.3

--- src/assets/js/windows/updateWindow.js
+++ src/assets/js/windows/updateWindow.js
@@ -23,7 +23,7 @@ function destroyWindow() {
 function createWindow() {
     destroyWindow();
     updateWindow = new BrowserWindow({
-        title: "Aderi Launcher",
+        title: "Squid Craft Launcher",
         width: 400,
         height: 500,
         resizable: false,
